180f31a90027b2c88e5ed91fcf3e3e829c52291c
pranto <kumar.pranto@northsouth.edu>
2020-11-18 09:23:26 +0600

    Lab assessment submitted

---
+++ Lab Assessment Submission/Lab 2/1711783042(Lab_2).asm
@@ -0,0 +1,29 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+
+K1 EQU 3
+K2 EQU 5
+K3 EQU 7
+    
+            
+MOV AX, K1
+ 
+ADD AX, K2
+
+ADD AX, K3
+
+MOV BX, AX      
+   
+
+
+
+ret
+
+
+
+
+                                                 
